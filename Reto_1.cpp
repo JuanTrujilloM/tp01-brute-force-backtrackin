1390bcce73ad8ef6eb002a828ce2e6ae09b3e69c
Andres Perez Quinchia <aperezq@eafit.edu.co>
2026-04-05 00:01:14 -0500

    Agregada funcionalidad de medir los tiempos

--- Reto_1.cpp
+++ Reto_1.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <chrono>
 
 using namespace std;
 
-void resolverPermutaciones(vector<int> A, bool imprimir_listado) {
+
+struct Resultado {
+    long long total_generadas;
+    long long total_validas;
+    double tiempo_ms;
+};
+
+Resultado resolverPermutaciones(vector<int> A, bool imprimir_listado) {
     sort(A.begin(), A.end());
     
     long long total_generadas = 0;
     long long total_validas = 0;
+
+    auto inicio = chrono::high_resolution_clock::now();
 
     do {
         total_generadas++;
@@ -32,14 +42,51 @@ void resolverPermutaciones(vector<int> A, bool imprimir_listado) {
         }
     } while (next_permutation(A.begin(), A.end()));
 
+    auto fin = chrono::high_resolution_clock::now();
+    double tiempo_ms = chrono::duration<double, milli>(fin - inicio).count();
+
     cout << "total_generadas = " << total_generadas << "\n";
     cout << "total_validas   = " << total_validas << "\n";
+    cout << "Tiempo: " << tiempo_ms << " ms\n\n";
+
+    return {total_generadas, total_validas, tiempo_ms};
 }
 
 int main() {
     vector<int> A = {1, 3, 4};
-    cout << "Entrada: n = " << A.size() << ", A = {1, 3, 4}\n";
+    cout << "Entrada: n = " << A.size() << ", A = {1, 3, 4}\n \n";
     resolverPermutaciones(A, true);
     
+    cout << "Medicion experimental de tiempos\n";
+
+    vector<int> valores_n = {5, 8, 10, 11, 12, 13};
+    vector<Resultado> resultados;
+
+    for (int n : valores_n) {
+        vector<int> conjunto(n);
+        for (int i = 0; i < n; i++) conjunto[i] = i + 1;
+
+        long long fact = 1;
+        for (int i = 1; i <= n; i++) fact *= i;
+        cout << "\nn = " << n << " (n! = " << fact << ")\n";
+
+        Resultado r = resolverPermutaciones(conjunto, false);
+        resultados.push_back(r);
+    }
+
+    cout << "\n------------------------------------------\n";         
+    cout << "          TABLA RESUMEN DE TIEMPOS        \n";
+    cout << "------------------------------------------\n";
+    cout << "  n  | Permutaciones |  Validas | Tiempo (ms)\n";
+    cout << "-----|---------------|----------|------------\n";
+    for (size_t i = 0; i < valores_n.size(); i++) {
+        printf("  %-2d | %13lld | %8lld | %10.2f\n",
+               valores_n[i],
+               resultados[i].total_generadas,
+               resultados[i].total_validas,
+               resultados[i].tiempo_ms);
+    }
+    cout << "------------------------------------------\n";
+
     return 0;
 }
